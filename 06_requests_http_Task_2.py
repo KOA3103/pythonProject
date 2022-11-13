a79fcdd6837065efaa162af6066798ef271882b6
Oleg Karbaev <koa3103@gmail.com>
2022-11-14 00:33:17 +0300

    Домашнее задание по теме "Работа с библиотекой requests, http-запросы" Задача №2 Программа, которая принимает на вход путь до файла на компьютере и сохраняет на Яндекс.Диск с таким же именем.

--- 06_requests_http_Task_2.py
+++ 06_requests_http_Task_2.py
@@ -1,11 +1,13 @@
 import requests
 from my_token import token
 from pprint import pprint
-import json
-import collections
 from pathlib import Path
 
-
+"""
+Работа с библиотекой requests, http-запросы.
+Программа, которая принимает на вход путь до файла на компьютере
+и сохраняет на Яндекс.Диск с таким же именем.
+"""
 class YaUploader:
     URL_FILES_LIST: str = 'https://cloud-api.yandex.net/v1/disk/resources/files'
     URL_FILES_RESOURCES: str = 'https://cloud-api.yandex.net/v1/disk/resources'
@@ -21,39 +23,54 @@ class YaUploader:
         pprint(json_info)
         return
 
-    def _get_upload_link(self, ya_disk_path: str = ''):
+    def _get_upload_link(self, ya_disk_path: str = "new"):  # Думал что тут можно выбрать куда загружать файлы.
         params = {"path": ya_disk_path, "overwrite": "true"}
         response = requests.get(self.URL_UPLOAD_LINK, headers=self.header, params=params)
         upload_url = response.json().get("href")
         return upload_url
 
     def upload(self, file_path: str):
-        list_files = list(Path('upload_folder').glob("*.txt"))
+
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+
+        list_files = list(Path(file_path).glob("*.*"))  # тут можно выбрать какие файлы загружать.
         for i in list_files:
             file_name = str(i).split('\\')
-            upload_link = self._get_upload_link(file_name[1])
-            with open(file_name[1], 'rb') as file_obj:
+            # Запрос URL для загрузки.
+            upload_link = self._get_upload_link(file_name[-1])
+            with open(i, 'rb') as file_obj:
+                # Загрузка файла на полученный URL
                 response = requests.put(upload_link, data=file_obj)
                 if response.status_code == 201:
-                    print(f"Файл из {file_path} успешно загружен на Яндекс.Диск\n"
-                          f" Статус код: {response.status_code}")
+                    print(f"Файл <{file_name[-1]}>\n  из <{file_path}> \n   загружен на Яндекс.Диск.\n")
+        return
+
+    def create_folder_into_YaDisk(self, ya_disk_path):
+        create_folder = requests.put(self.URL_FILES_RESOURCES, headers=self.header, params={"path": ya_disk_path})
+        if create_folder.status_code == 201:
+            print(f"Папка <{ya_disk_path}> создана! Статус {create_folder.status_code}.")
+        else:
+            print(f'{create_folder.json().get("message")} Статус {create_folder.status_code}.')
         return
 
     def delete_file_folder(self, ya_disk_path: str):
         params = {"path": ya_disk_path, "permanently": "true"}
         response = requests.delete(self.URL_FILES_RESOURCES, headers=self.header, params=params)
         if response.status_code == 204 or response.status_code == 202:
-            print(f"Успешно удален {ya_disk_path}. Статус {response.status_code}")
+            print(f"Успешно удален {ya_disk_path}. Статус {response.status_code}.")
         return
 
 
 uploader = YaUploader(token)
 
-# Загружает все файлы из папки проекта upload_folder.
-uploader.upload("upload_folder")
+# Загружает все файлы из папки проекта upload_folder можно выбоать др. указав путь
+uploader.upload('upload_folder')
+
+# Создать папку.
+# uploader.create_folder_into_YaDisk("new")
 
 # Плоский список всех файлов.
-# uploader.get_flat_list_of_all_files( )
+# uploader.get_flat_list_of_all_files()
 
 # Удаляет файл.
-uploader.delete_file_folder('1.txt')
+# uploader.delete_file_folder('1.txt')
